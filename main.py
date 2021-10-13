61ef14310480699accbcc7af9165aade5460b985
ThunderingWest4 <45630356+ThunderingWest4@users.noreply.github.com>
2021-10-13 19:01:53 -0400

    based

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import discord
+from datetime import datetime
 
 with open('secrets.json') as f:
     secrets = json.load(f)
@@ -8,13 +9,13 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f"Logged in as {client.user}")
+    print(f"Logged in as {client.user} at time {datetime.now()}")
 
 @client.event
 async def on_message(msg):
     if msg.content.startswith('$ping'):
         await msg.channel.send('pong')
-    elif msg.content.lower().startswith('based'):
+    elif ('based' in msg.content.lower().split(" ")) and not ('based on' in msg.content.lower()):
         await msg.channel.send(f"> {msg.content} \nbased? based on what")
 
 
