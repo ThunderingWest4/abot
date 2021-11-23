7b30a30cf86dcb2b289bd640b426a22fe6c6f907
ThunderingWest4 <45630356+ThunderingWest4@users.noreply.github.com>
2021-11-22 23:58:20 -0500

    yay

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 import discord
 from datetime import datetime
-import string
+import random
 
 with open('secrets.json') as f:
     secrets = json.load(f)
@@ -15,11 +15,18 @@ async def on_ready():
 @client.event
 async def on_message(msg):
     cont = msg.content.lower()
-    cont = cont.translate(str.maketrans('', '', string.punctuation))
+    cont = cont.translate(str.maketrans('', '', "!#$%^&()?[]{}\\/.,"))
+    cont = cont.replace("\n", "\n>")
+
     if msg.content.startswith('$ping'):
         await msg.channel.send('pong')
-    elif ('based' in cont.split(" ")) and not (('based on' in cont) or ('based in' in cont)):
-        await msg.channel.send(f"> {msg.content} \nbased? based on what")
+    elif "@everyone" in cont or "@here" in cont:
+        await msg.channel.send("hah you thought")
+    elif ('based' in cont.split(" ")) and not ('based on ' in cont) and not ('based in ' in cont) and not ('based upon ' in cont) and not ('based off' in cont):
+        options = ["based? based on what?", "more like BASED on deez nuts"]
+        probs = [0.75, 0.25]
+        addition = random.choices(options, weights=probs, k=1)[0]
+        await msg.channel.send(f"> {msg.content} \n{addition}")
 
 
 client.run(secrets['token'])
